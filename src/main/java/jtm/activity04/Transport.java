4b8cc0faa2353035501742535b30c06c284de38c
jorenchik <jorens.stekels@gmail.com>
2025-03-25 19:34:50 +0200

    activity04

--- src/main/java/jtm/activity04/Transport.java
+++ src/main/java/jtm/activity04/Transport.java
@@ -6,9 +6,9 @@ public class Transport {
     // Do not change access modifiers to encapsulate internal properties!
 
     // TODO #1 make these fields final
-    private String id = null; // Transport registration number
-    private float consumption = 0; // fuel consumption in litres per 100km
-    private int tankSize = 0; // tank size in litres
+    private final String id; // Transport registration number
+    private final float consumption; // fuel consumption in litres per 100km
+    private int tankSize; // tank size in litres
     private float fuelInTank; // fuel in tank
 
     /*- TODO #2
@@ -18,6 +18,13 @@ public class Transport {
      * And make fuel tank full.
      */
 
+    public Transport(String id, float consumption, int tankSize) {
+		super();
+		this.id = id;
+		this.consumption = consumption;
+		this.tankSize = tankSize;
+		this.fuelInTank = tankSize;
+	}
 
     /*- TODO #3
      * Select menu: Source — Generate getters and Setters...
@@ -25,6 +32,39 @@ public class Transport {
      * fuelInTank fields
      */
 
+	public String getId() {
+		return id;
+	}
+
+//	public void setId(String id) {
+//		this.id = id;
+//	}
+
+	public float getConsumption() {
+		return consumption;
+	}
+
+//	public void setConsumption(float consumption) {
+//		this.consumption = consumption;
+//	}
+
+	public int getTankSize() {
+		return tankSize;
+	}
+
+//	public void setTankSize(int tankSize) {
+//		this.tankSize = tankSize;
+//	}
+
+	public float getFuelInTank() {
+		return fuelInTank;
+	}
+
+//	public void setFuelInTank(float fuelInTank) {
+//		this.fuelInTank = fuelInTank;
+//	}
+    
+    
     /*- TODO #4
      * Select menu: Source — Generate toString()...
      * and implement this method, that returns String in form:
@@ -33,16 +73,23 @@ public class Transport {
      * HINT: use String.format(Locale.US, "%.2f", float) to limit shown digits
      * to 2 decimal for fractions, and dot as a decimal delimiter.
      */
-
+	public String toString() {
+		return String.format(
+		    Locale.US,
+			"Id:%s cons:%.1fl/100km, tank:%dl, fuel:%.2fl", 
+			id, consumption, tankSize, fuelInTank
+		);
+	}
 
     // Return transport id and type as string e.g. "AAA Transport"
     // HINT: use this.getClass().getSimpleName(); to get type of transport
     protected final String getType() {
         // TODO #5 return required value
-        return "";
+        return String.format("%s %s", id, this.getClass().getSimpleName());
     }
 
-    // HINT: use getType() to describe transport and road.toString() to describe
+
+	// HINT: use getType() to describe transport and road.toString() to describe
     // road
     // HINT: String.format(Locale.US, "%.2f", float) to format float number with
     // fixed mask
@@ -53,7 +100,20 @@ public class Transport {
         // TODO #7 If there is no enough fuel in tank, return string in form:
         // "Cannot move on From–To, 180km. Necessary
         // fuel:0.00l, fuel in tank:0.00l"
-        return "";
+    	
+    	float necessaryFuel = (float) road.getDistance() * (consumption / 100);
+    	if (fuelInTank >= necessaryFuel) {
+    		fuelInTank -= necessaryFuel;
+			return String.format(
+				"%s is moving on %s — %s, %dkm",
+				getType(), road.getFrom(), road.getTo(), road.getDistance()
+			);
+		} else {
+			return String.format(
+				Locale.US,
+				"Cannot move on %s — %s, %dkm. Necessary fuel:%.2fl, fuel in tank:%.2fl",
+				road.getFrom(), road.getTo(), road.getDistance(), necessaryFuel, fuelInTank
+			);
+		}
     }
-
 }
